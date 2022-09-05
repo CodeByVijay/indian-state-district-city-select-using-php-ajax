870dc9f03621747ecabe7122a2940fc2f0ae6fc2
Vijay Amule <gitbyvijay@gmail.com>
2022-09-05 16:03:09 +0530

    live search

---
+++ live_state_search.php
@@ -0,0 +1,92 @@
+<?php
+require_once('main.php');
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Practice JQuery</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<style>
+    #stateData>ul>li {
+        list-style: none;
+    }
+</style>
+
+<body>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="text-center text-primary">Select State District City</h4>
+            </div>
+            <div class="card-body">
+                <form action="javascript:void(0)" id="formVal">
+
+
+                    <div class="form-group">
+                        <label for="">Search State</label>
+                        <input type="text" name="state" data-id="" id="state" class="form-control">
+                    </div>
+                    <div id="stateData"></div>
+
+                    <button type="submit" class="btn btn-success" onclick="formsubmit(this)">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+<script>
+    $('#state').on('keyup', function() {
+        $('#stateData').html('')
+        $('#stateData').show();
+        let keyword = $(this).val()
+        if (keyword !== "") {
+            $.ajax({
+                type: "GET",
+                url: "main.php",
+                data: {
+                    'keyword': keyword
+                },
+                success: function(result) {
+                    // console.log(result)
+                    let html = "";
+                    $(JSON.parse(result)).each(function(index, val) {
+                        html += `<ul><li class="data" data-id="${val.id}">${val.name}</li></ul>`;
+                    })
+                    $('#stateData').html(html)
+                }
+            });
+        }
+    })
+
+    $(document).on('click', ".data", function() {
+        let getData = $(this).html()
+        let getId = $(this).data('id')
+        $('#state').val(getData);
+        $('#state').attr("data-id", getId)
+        $('#stateData').html('');
+        $('#stateData').hide();
+
+    })
+
+    function formsubmit(e) {
+        e.preventDefault
+        let form = $('#formVal')
+        let formData = form.serialize();
+        console.log(formData);
+    }
+</script>
--- main.php
+++ main.php
@@ -12,9 +12,9 @@ if (isset($_GET['state_id']) && !empty($_GET['state_id'])) {
     $district_query = mysqli_query($conn, $district);
 
     $html = [];
-    while($row=mysqli_fetch_assoc($district_query)){
-        $arr = ['id'=>$row['districtid'],'name'=>$row['district_title']];
-        array_push($html,$arr);
+    while ($row = mysqli_fetch_assoc($district_query)) {
+        $arr = ['id' => $row['districtid'], 'name' => $row['district_title']];
+        array_push($html, $arr);
     }
     echo json_encode($html);
 }
@@ -25,9 +25,28 @@ if (isset($_GET['district_id']) && !empty($_GET['district_id'])) {
     $city_query = mysqli_query($conn, $city);
 
     $html = [];
-    while($row=mysqli_fetch_assoc($city_query)){
-        $arr = ['id'=>$row['id'],'name'=>$row['name']];
-        array_push($html,$arr);
+    while ($row = mysqli_fetch_assoc($city_query)) {
+        $arr = ['id' => $row['id'], 'name' => $row['name']];
+        array_push($html, $arr);
     }
+    echo json_encode($html);
+}
+
+if (isset($_GET['keyword']) && !empty($_GET['keyword'])) {
+    $keyword = $_GET['keyword'];
+    $state = "SELECT * FROM `state` WHERE `state_title` LIKE '$keyword%' LIMIT 5";
+    $state_query = mysqli_query($conn, $state);
+
+    $html = [];
+    if (mysqli_num_rows($state_query) > 0) {
+        while ($row = mysqli_fetch_assoc($state_query)) {
+            $arr = ['id' => $row['state_id'], 'name' => $row['state_title']];
+            array_push($html, $arr);
+        }
+    } else {
+        $html = ['name' => "No Records Found."];
+    }
+
+
     echo json_encode($html);
 }
